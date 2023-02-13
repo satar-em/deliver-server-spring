08dc10f2c1f716dbdc0fb7e876ef3d425eab6014
satar2 <satar2@artemis.com>
2023-02-13 05:56:48 -0800

    init

---
+++ src/main/kotlin/com/emami/deliver/DeliverApplication.kt
@@ -0,0 +1,14 @@
+package com.emami.deliver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliverApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliverApplication>(*args)
+
+
+
+}
---
+++ src/test/kotlin/com/emami/deliver/DeliverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.emami.deliver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
